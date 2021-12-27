32d01293e8e37dbd84d84c9a86d73b272038fefa
Yang-Yefeng <18B904013@stu.hit.edu.cn>
2021-12-27 22:32:48 +0800

    Create env_test.py

---
+++ environment/envs/env_test.py
@@ -0,0 +1,20 @@
+import random
+from environment.envs.flight_attitude_simulator import Flight_Attitude_Simulator
+
+
+'''Flight Attitude Simulator Test'''
+def test_flight_attitude_simulator():
+    env = Flight_Attitude_Simulator(initTheta=0.0, setTheta=0.)
+    test_num = 10
+    for _ in range(test_num):
+        env.reset_random()
+        while not env.is_terminal:
+            env.show_dynamic_image(isWait=False)
+            # action = random.choice(env.action_space[0])
+            action = 0.0
+            env.current_state, env.current_action[0], env.reward, env.next_state, env.is_terminal = env.step_update(action=action)
+    # env.saveData(is2file=True, filepath='../../datasave/')
+
+
+test_flight_attitude_simulator()
+'''Flight Attitude Simulator Test'''
