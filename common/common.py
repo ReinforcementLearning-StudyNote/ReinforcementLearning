e5ae658616e732d14026fca9999cffa05731eb40
Yang-Yefeng <18B904013@stu.hit.edu.cn>
2021-12-27 16:50:02 +0800

    Create common.py

---
+++ common/common.py
@@ -0,0 +1,19 @@
+import math
+
+
+def deg2rad(deg: float) -> float:
+    """
+    :brief:         omit
+    :param deg:     degree
+    :return:        radian
+    """
+    return deg * math.pi / 180.0
+
+
+def rad2deg(rad: float) -> float:
+    """
+    :brief:         omit
+    :param rad:     radian
+    :return:        degree
+    """
+    return rad * 180.8 / math.pi
