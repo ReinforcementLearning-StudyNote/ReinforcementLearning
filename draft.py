8449e3a0cfa2339f5521b2df50b190c2128135f2
YANG-Yefeng <21037385r@connect.polyu.hk>
2022-02-27 22:44:55 +0800

    yyf

--- draft.py
+++ draft.py
@@ -10,8 +10,6 @@ import torch
 import pandas as pd
 import matplotlib.pyplot as plt
 from environment.envs.pathplanning.bezier import Bezier
-# import emoji
-import turtle
 
 
 if __name__ == '__main__':
@@ -19,5 +17,30 @@ if __name__ == '__main__':
     # a = GaussianNoise(mu=np.zeros(1))
     # b = torch.clip(torch.tensor(a(sigma=1.0)), -clip, clip)
     # print(b)
-    print(random.randint(0, 1))
+    # print(random.randint(0, 1))
+    a = torch.rand((10000, 10000))
+    b = torch.rand((10000, 10000))
+    print('Size a:', a.size())
+    print('Size b:', b.size())
+    t0 = time.time()
+    for _ in range(100):
+        c = torch.matmul(a, b)
+    t1 = time.time()
+    print('Device:', a.device, '  Time:', t1 - t0)
+
+    print('Start moving matrix to GPU...')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    t0 = time.time()
+    a = a.to(device)
+    b = b.to(device)
+    t1 = time.time()
+    print('time:', t1 - t0)
+    print('Finish moving matrix to GPU...')
+
+    t0 = time.time()
+    for _ in range(100):
+        c = torch.matmul(a, b)
+    t1 = time.time()
+
+    print('Device:', a.device, '  Time:', t1 - t0)
     pass
